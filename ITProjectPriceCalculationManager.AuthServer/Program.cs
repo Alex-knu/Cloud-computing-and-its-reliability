43a3a1f27d9420b1a04148e5557ec41864241499
Alex Rozdobudko <arozdobudko2@gmail.com>
2024-03-23 16:29:43 +0200

    fix

--- ITProjectPriceCalculationManager.AuthServer/Program.cs
+++ ITProjectPriceCalculationManager.AuthServer/Program.cs
@@ -62,16 +62,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+// using (var scope = app.Services.CreateScope())
+// {
+//     var services = scope.ServiceProvider;
 
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    if (context.Database.GetPendingMigrations().Any())
-    {
-        context.Database.Migrate();
-    }
-}
+//     var context = services.GetRequiredService<ApplicationDbContext>();
+//     if (context.Database.GetPendingMigrations().Any())
+//     {
+//         context.Database.Migrate();
+//     }
+// }
 
 app.UseStaticFiles();
 app.UseCors(
